3d75c828993590a7f1ed721a824a012e47e89f89
BuiVanLinh <linhbv@htacive.com>
2020-05-17 11:39:07 +0700

    'fix-loading-baidang'

--- src/views/BaiDang/Modal_Create_BaiDang.vue
+++ src/views/BaiDang/Modal_Create_BaiDang.vue
@@ -147,7 +147,7 @@
                 <div class="col-md-4">
                   <div class="row">
                     <div class="col-md-6">
-                      <button class="btn btn-success" @click.prevent="create()">Thêm mới</button>
+                      <button class="btn btn-success" @click.prevent="create()"><span v-if="onLoading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Thêm mới</button>
                     </div>
                     <div class="col-md-6">
                       <button
